0fdffa86c0f0e2f8867d4d9cb54f8d53daa4bd7d
Yeongjin Jang <yeongjinjanggrad@gmail.com>
2018-01-07 18:09:35 -0800

    fetch and setup

--- vm_data/user_setup.sh
+++ vm_data/user_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cp /host_data/id_ecdsa* /home/ubuntu/.ssh
+chmod 400 /home/ubuntu/.ssh/id_ecdsa
 mkdir /home/ubuntu/bin
 echo "export PATH=\$PATH:/home/ubuntu/bin" >> /home/ubuntu/.bashrc
 cp /host_data/connect /home/ubuntu/bin
