875116f6deb2ddc57cc30076c28659a61abe87b7
Yeongjin Jang <yeongjinjanggrad@gmail.com>
2018-01-17 15:52:19 -0800

    x change script to python (to check $HOME for user vagrant/ubuntu)

--- vm_data/user_setup.py
+++ vm_data/user_setup.py
@@ -2,23 +2,32 @@
 
 import os
 
-git clone https://github.com/cs519-osu/peda.git /home/ubuntu/peda
-echo "source ~/peda/peda.py" >> /home/ubuntu/.gdbinit
-echo "PEDA install DONE!"
+def main():
+    UN = 'ubuntu'
+    if os.path.exists('/home/vagrant/.ssh'):
+        UN = 'vagrant'
 
-git clone --depth=1 https://github.com/amix/vimrc.git /home/ubuntu/.vim_runtime
-HOME=/home/ubuntu sh /home/ubuntu/.vim_runtime/install_awesome_vimrc.sh
+    os.system("git clone https://github.com/cs519-osu/peda.git /home/%s/peda" % UN)
+    os.system('echo "source ~/peda/peda.py" >> /home/%s/.gdbinit' % UN)
+    print("PEDA install DONE!")
 
-curl -R https://raw.githubusercontent.com/DenisCarriere/.bashrc/master/.bashrc >> /home/ubuntu/.bashrc
-echo >> /home/ubuntu/.vimrc; echo "set t_Co=256" >> /home/ubuntu/.vimrc
+    os.system("git clone --depth=1 https://github.com/amix/vimrc.git /home/%s/.vim_runtime" % UN)
+    os.system("HOME=/home/%s sh /home/ubuntu/.vim_runtime/install_awesome_vimrc.sh" % UN)
+
+    os.system("curl -R https://raw.githubusercontent.com/DenisCarriere/.bashrc/master/.bashrc >> /home/%s/.bashrc" % UN)
+    os.system('echo >> /home/%s/.vimrc; echo "set t_Co=256" >> /home/%s/.vimrc' % (UN, UN))
 
 
-chown -R ubuntu:ubuntu /home/ubuntu
 
 
-cp /host_data/id_ecdsa* /home/ubuntu/.ssh
-chmod 400 /home/ubuntu/.ssh/id_ecdsa
-mkdir /home/ubuntu/bin
-echo "export PATH=\$PATH:/home/ubuntu/bin" >> /home/ubuntu/.bashrc
-cp /host_data/connect /home/ubuntu/bin
-cp /host_data/fetch /home/ubuntu/bin
+    os.system("cp /host_data/id_ecdsa* /home/%s/.ssh" % UN)
+    os.system("chmod 400 /home/%s/.ssh/id_ecdsa" % UN)
+    os.system("mkdir /home/%s/bin" % UN)
+    os.system('echo "export PATH=\$PATH:/home/%s/bin" >> /home/%s/.bashrc' % (UN, UN))
+    os.system("cp /host_data/connect /home/%s/bin" % UN)
+    os.system("cp /host_data/fetch /home/%s/bin" % UN)
+
+    os.system("chown -R %s:%s /home/%s" % (UN, UN, UN))
+
+if __name__ == '__main__':
+    main()
